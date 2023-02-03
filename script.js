833e3b5db367064794c24c74cd9affacd23e9dfe
Modder65 <119975910+Modder65@users.noreply.github.com>
2023-02-03 16:19:58 -0500

    Added another function to include 5 rounds of the game, and present a winner or loser after calculating the player and computer scores

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-let playerChoice = prompt("Choose Rock, Paper, or Scissors");
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     let choice = Math.random() * 3;
@@ -18,20 +19,42 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection.toLowerCase() == computerSelection.toLowerCase()) {
         alert("It's a tie!");
     } else if (playerSelection.toLowerCase() == "rock" && computerSelection == "Paper") {
-        alert("Paper beats Rock, you lose!");
+        alert("You lose! Paper beats Rock");
+        computerScore++;
     } else if (playerSelection.toLowerCase() == "rock" && computerSelection == "Scissors") {
-        alert("Rock beats Scissors, you win!");
+        alert("You win! Rock beats Scissors");
+        playerScore++;
     } else if (playerSelection.toLowerCase() == "paper" && computerSelection == "Rock") {
-        alert("Paper beats Rock, you win!");
+        alert("You win! Paper beats Rock");
+        playerScore++;
     } else if (playerSelection.toLowerCase() == "paper" && computerSelection == "Scissors") {
-        alert("Scissors beats Paper, you lose!");
+        alert("You lose! Scissors beats Paper");
+        computerScore++;
     } else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "Rock") {
-        alert("Rock beats scissors, you lose!");
+        alert("You lose! Rock beats scissors");
+        computerScore++;
     } else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "Paper") {
-        alert("Scissors beats Paper, you win!");
+        alert("You win! Scissors beats Paper");
+        playerScore++;
     }
 }
 
 
-getComputerChoice();
-playRound(playerChoice, getComputerChoice());
+function game() {
+    for (let i = 0; i < 5; i++) {
+        let playerChoice = prompt("Choose Rock, Paper, or Scissors");
+        getComputerChoice();
+        playRound(playerChoice, getComputerChoice());
+    }
+    if (playerScore > computerScore) {
+        alert("You win! You won more rounds than the computer");
+    } else if (playerScore < computerScore) {
+        alert("You lose! You lost more rounds than the computer");
+    } else if (playerScore === computerScore) {
+        alert("Nobody wins! You tied against the computer!");
+    } 
+}
+
+game()
+
+
